a9d768a2ae678ef07eb37cb32cc030a5a1ccdfe5
fullyalive <wogud1202@gmail.com>
2019-03-06 00:02:23 +0900

    ClientState | schema, type 정의

--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,26 @@
-export const resolvers = {};
-export const defaults = {};
+export const defaults = {
+    notes: []
+};
+export const resolvers = [
+    // note: (_, variables,  { cache, getCacheKey})
+    `
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+    type Mutation {
+        createNote(title: String!, content: String!, isImportant: Boolean!)
+        editNote(id: String!, title: String!, content: String!)
+    }
+    type Note {
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
 export const typeDefs = {};
